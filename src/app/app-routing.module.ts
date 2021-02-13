15d4050c2130699dba5f8961479f640949f3f659
NAGARRO\ashwani01 <ashwani01@nagarro.com>
2021-02-13 17:27:20 +0530

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
   { path: 'category/:categoryName', component: ProductListComponent },
+  { path: 'cart', component: CartComponent },
   { path: '**', component: ProductListComponent }
 ];
 
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -11,21 +11,18 @@ import { Subscription } from "rxjs";
   templateUrl: "./cart.component.html"
 })
 export class CartComponent implements OnInit, OnDestroy {
+
   public products: Observable<Product[]>;
   public cart: Cart;
   public itemCount: number;
-
   private cartSubscription: Subscription;
 
-  public constructor(private CartService: CartService) {
-  }
-
-
+  public constructor(private CartService: CartService) { }
 
   public ngOnInit(): void {
     this.cartSubscription = this.CartService.getCartById().subscribe((Cart: any) => {
       this.cart = Cart;
-       this.itemCount = 100;
+      this.itemCount = 100;
       console.log("!!!!!!!" + this.cart);
     });
   }
