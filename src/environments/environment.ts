6041a795f7953f84dc07ce2f98e5bbb2114d7fc8
gauravcs107 <2009cs107@gmail.com>
2021-02-22 23:53:50 +0530

    new ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   inventoryServiceURL: 'http://35.224.251.195/im/inventory',
   cartServiceURL: 'http://104.197.155.135/ct/cart/',
-  productServiceURL: 'http://10.4.0.57:8090/',
+  productServiceURL: 'http://10.4.0.57/',
   orderServiceURL: 'http://104.197.155.135/ct/order/',
-  productListingServiceURL: 'http://10.4.0.57:8090/',
+  productListingServiceURL: 'http://10.4.0.57/',
 
   // inventoryServiceURL: 'http://35.193.47.77/inventory/im/inventory',
   // cartServiceURL: 'http://35.193.47.77/cart/ct/cart/',
